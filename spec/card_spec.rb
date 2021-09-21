bd752ed06a5a6a2cbf8de60e41bc12d0f8991bdc
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-22 00:11:52 +0900

    update

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,10 +7,7 @@ class Card
 end
 
 RSpec.describe Card do 
-  def card 
-    p 'card'
-    Card.new("Ace", "Spades")
-  end
+  let(:card) { Card.new("Ace", "Spades")}
 
   it "has a rank and that rank change" do
     expect(card.rank).to eq("Ace")
