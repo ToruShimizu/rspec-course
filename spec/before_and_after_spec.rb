3543b008d03fa198bbaf539b9e4b8edc31e185a0
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-22 23:06:37 +0900

    feat: before_afterのテストを追加

---
+++ spec/before_and_after_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe 'before and after hooks' do
+  before(:example) do
+    puts 'Before example'
+  end
+
+  after(:example) do
+    puts 'After example'
+  end
+
+  before(:context) do
+    puts 'Before context'
+  end
+
+  after(:context) do
+    puts 'After context'
+  end
+
+  it 'is just a random example' do
+    expect(5 * 4).to eq(20)
+  end
+
+  it 'is just anothe random example' do
+    expect(3 - 2).to eq(1)
+  end
+
+end
