fffc0001fda226b9523f8e75f7a8757737ef99ce
ToruShimizu <nobbyz.t.nn@gmail.com>
2021-09-24 00:12:39 +0900

    feat: メソッドの呼び出し回数テストを追加

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -34,6 +34,7 @@ class Movie
       actor.act
       actor.fall_off_ladder
       actor.light_on_fire
+      actor.act
     end
   end
 end
@@ -49,10 +50,16 @@ RSpec.describe Movie do
 
   describe '#start_shooting method' do
     it 'expects an actor to do 3 actions' do
-      expect(stuntman).to receive(:ready?)
-      expect(stuntman).to receive(:act)
-      expect(stuntman).to receive(:fall_off_ladder)
-      expect(stuntman).to receive(:light_on_fire)
+      # expecr(stuntman).to receive(:light_on_fire).once
+      # expecr(stuntman).to receive(:light_on_fire).exactly(1).times
+      expect(stuntman).to receive(:light_on_fire).at_most(1).times
+      # expect(stuntman).to receive(:ready?)
+      # expect(stuntman).to receive(:act)
+      # expect(stuntman).to receive(:act).twice
+      # expect(stuntman).to receive(:act).exactly(2).times
+      expect(stuntman).to receive(:act).at_least(2).times
+      # expect(stuntman).to receive(:fall_off_ladder)
+      # expect(stuntman).to receive(:light_on_fire)
       subject.start_shoooting
     end
   end
